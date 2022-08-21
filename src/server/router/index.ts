b3d63b22b0e31043e2d4849d5702b79b7d6c4c00
anlett10 <anlewba@gmail.com>
2022-08-21 23:23:43 +0700

    add auth router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,8 +1,10 @@
 // src/server/router/index.ts
 
 import { t } from "./utils";
+import { authRouter } from "./subrouters/auth";
 import { health } from './subrouters/health';
 export const appRouter = t.router({
+    auth: authRouter,
     health
 });
 
