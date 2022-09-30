7420abf0de47797ed3a5935179c3100c706e0078
anlett10 <anlewba@gmail.com>
2022-09-30 22:26:33 +0700

    fix app_

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,18 @@
 import "~/styles/globals.css";
-import { NextPage } from "next";
-import { AppProps } from "next/app";
 import { AppType } from "next/dist/shared/lib/utils";
-import { ReactElement, ReactNode } from "react";
-import { DefaultLayout } from "../components/DefaultLayout";
 import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
 import { trpc } from "../utils/trpc";
 
-export type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-const MyApp = (({
+const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
-}: AppPropsWithLayout) => {
-  const getLayout =
-    Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
-
-  return getLayout(
+}) => {
+  return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
-}) as AppType;
+};
 
 export default trpc.withTRPC(MyApp);
